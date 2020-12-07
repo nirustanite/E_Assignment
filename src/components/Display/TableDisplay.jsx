92e16cdc48c2abc226ef2354901ad899e8b6005d
Nirupamaa Jaya <nirustanite@gmail.com>
2020-12-08 00:10:42 +0100

    .

--- src/components/Display/TableDisplay.jsx
+++ src/components/Display/TableDisplay.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Table, Header, Image, Icon, Button } from 'semantic-ui-react';
 import styled from 'styled-components';
+import './TableDisplay.css';
 
 const StyledButton = styled(Button)`
     background-color: white !important;
@@ -18,7 +19,7 @@ const Display = (props) => {
             <Table basic="very">
                 <Table.Header>
                     <Table.Row>
-                        <Table.HeaderCell>Name</Table.HeaderCell>
+                        <Table.HeaderCell scope="col">Name</Table.HeaderCell>
                         <Table.HeaderCell>Type</Table.HeaderCell>
                         <Table.HeaderCell>Status</Table.HeaderCell>
                         <Table.HeaderCell>Owner</Table.HeaderCell>
@@ -29,15 +30,13 @@ const Display = (props) => {
                 <Table.Body>
                     {listOfItems.length >=1 && listOfItems.map((item,i) => {
                         return <Table.Row key={i} verticalAlign="middle">
-                                <Table.Cell>
-                                    <StyledButton icon >
-                                        <u style={{ color: "#089ec8"}}>{item.name}</u>
-                                    </StyledButton>     
+                                <Table.Cell data-label="Name">
+                                   {item.name}
                                 </Table.Cell>
-                                <Table.Cell>
+                                <Table.Cell data-label="Type">
                                     {item.type}
                                 </Table.Cell>
-                                <Table.Cell>
+                                <Table.Cell data-label="Status">
                                     {item.status === "Progress" ? (
                                         <>
                                         <Icon name='hourglass end' color="brown" /> &nbsp; {item.status}
@@ -48,7 +47,7 @@ const Display = (props) => {
                                         </>
                                     )}
                                 </Table.Cell>
-                                <Table.Cell>
+                                <Table.Cell data-label="Owner">
                                     <Header as='h4' image>
                                         <Image src={item.avatar} size='mini' />
                                         <Header.Content>
@@ -56,13 +55,50 @@ const Display = (props) => {
                                         </Header.Content>
                                     </Header>
                                 </Table.Cell>
-                                <Table.Cell>
+                                <Table.Cell data-label="Date">
                                     {item.date}
                                 </Table.Cell>
                         </Table.Row>
                     })}
                 </Table.Body>
             </Table>
+            {/* <table>
+            <caption>Statement Summary</caption>
+            <thead>
+                <tr>
+                <th scope="col">Account</th>
+                <th scope="col">Due Date</th>
+                <th scope="col">Amount</th>
+                <th scope="col">Period</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <td data-label="Account">Visa - 3412</td>
+                <td data-label="Due Date">04/01/2016</td>
+                <td data-label="Amount">$1,190</td>
+                <td data-label="Period">03/01/2016 - 03/31/2016</td>
+                </tr>
+                <tr>
+                <td scope="row" data-label="Account">Visa - 6076</td>
+                <td data-label="Due Date">03/01/2016</td>
+                <td data-label="Amount">$2,443</td>
+                <td data-label="Period">02/01/2016 - 02/29/2016</td>
+                </tr>
+                <tr>
+                <td scope="row" data-label="Account">Corporate AMEX</td>
+                <td data-label="Due Date">03/01/2016</td>
+                <td data-label="Amount">$1,181</td>
+                <td data-label="Period">02/01/2016 - 02/29/2016</td>
+                </tr>
+                <tr>
+                <td scope="row" data-label="Acount">Visa - 3412</td>
+                <td data-label="Due Date">02/01/2016</td>
+                <td data-label="Amount">$842</td>
+                <td data-label="Period">01/01/2016 - 01/31/2016</td>
+                </tr>
+            </tbody>
+            </table> */}
         </React.Fragment>
     );
 };
